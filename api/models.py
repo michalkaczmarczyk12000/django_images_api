b2bdfa55eabcf4185340a57594becac824e0823b
michal <m.kaczmarczyk12000@gmail.com>
2023-10-10 19:44:03 +0200

    created simple image and thumbnailSize models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL  # auth.User
+
+
+class ThumbnailSize(models.Model):
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.width}x{self.height}"
+
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='img')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.image}"
--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def home(request, *args, **kwargs):
+    return render(request)
